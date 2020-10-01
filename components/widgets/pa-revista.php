75aad58014932d0e5c51f40783176c9c6a7ab106
mizael isaltino <mizael.isaltino@gmail.com>
2020-10-01 16:10:57 -0300

    Iniciando a widget de revista

---
+++ components/widgets/pa-revista.php
@@ -0,0 +1,54 @@
+<div class="pa-widget pa-widget-revista col col-md-4 mb-5">
+	<h2>Revista</h2>
+	<div class="mt-4">
+		<div class="pa-glide-destaques d-flex align-items-center p-3">
+
+			
+			<div class="pa-slider-controle" data-glide-el="controls">
+				<i class="fas fa-angle-left fa-3x"></i>
+			</div>
+			
+
+			<div class="glide__track mx-3 my-4" data-glide-el="track">
+				<div class="glide__slides">
+					<div class="glide__slide">
+						<a href="#">	
+							<img class="rounded img-fluid" src="https://picsum.photos/336/390.webp?random=1"  alt="" >
+						</a>						
+					</div>
+					<div class="glide__slide">
+						<a href="#">	
+							<img class="rounded img-fluid" src="https://picsum.photos/336/390.webp?random=2"  alt="" >
+						</a>						
+					</div>
+					<div class="glide__slide">
+						<a href="#">	
+							<img class="rounded img-fluid" src="https://picsum.photos/336/390.webp?random=3"  alt="" >
+						</a>						
+					</div>
+					<div class="glide__slide">
+						<a href="#">	
+							<img class="rounded img-fluid" src="https://picsum.photos/336/390.webp?random=4"  alt="" >
+						</a>						
+					</div>
+					<div class="glide__slide">
+						<a href="#">	
+							<img class="rounded img-fluid" src="https://picsum.photos/336/390.webp?random=5"  alt="" >
+						</a>						
+					</div>				
+				</div>
+			</div>
+
+
+				
+			
+			<div class="pa-slider-controle" data-glide-el="controls">
+				<i class="fas fa-angle-right fa-3x"></i>
+			</div>
+
+		
+			
+
+		</div>
+	</div>
+</div>
