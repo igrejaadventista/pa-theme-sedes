c51bb753562b56ba0ccfebef8c5c6dd55eb3ba70
Eli Mendonça <contato@elimendonca.com>
2020-10-13 22:20:43 -0300

    - Move o slider da home p/ o tema filho - Adiciona filtro p/ carregar os campos do ACF do tema pai e tema filho

--- functions.php
+++ functions.php
@@ -96,3 +96,14 @@ function cconsole($var) {
 	echo "<script>console.log('" . $var . "');</script>";
 	return;
 }
+
+add_filter('acf/settings/save_json', function() {
+	return get_stylesheet_directory() . '/acf-json';
+});
+  
+add_filter('acf/settings/load_json', function($paths) {
+	if(is_child_theme()) {
+		$paths[] = get_template_directory() . '/acf-json';
+	}
+	return $paths;
+});
