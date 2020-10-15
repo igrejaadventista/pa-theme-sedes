39981debf14d4f9d4b51492ed422a274c5f4568f
Eli Mendonça <contato@elimendonca.com>
2020-10-15 10:16:21 -0300

    Instancia o ACF dentro do theme

--- functions.php
+++ functions.php
@@ -107,3 +107,22 @@ add_filter('acf/settings/load_json', function($paths) {
 	}
 	return $paths;
 });
+
+// Define path and URL to the ACF plugin.
+define( 'MY_ACF_PATH', get_template_directory() . '/acf/' );
+define( 'MY_ACF_URL', get_template_directory_uri() . '/acf/' );
+
+// Include the ACF plugin.
+include_once( MY_ACF_PATH . 'acf.php' );
+
+// Customize the url setting to fix incorrect asset URLs.
+add_filter('acf/settings/url', 'my_acf_settings_url');
+function my_acf_settings_url( $url ) {
+    return MY_ACF_URL;
+}
+
+// // (Optional) Hide the ACF admin menu item.
+// add_filter('acf/settings/show_admin', 'my_acf_settings_show_admin');
+// function my_acf_settings_show_admin( $show_admin ) {
+//     return true;
+// }
