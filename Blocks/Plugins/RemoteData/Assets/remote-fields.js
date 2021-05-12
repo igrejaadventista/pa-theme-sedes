1aedc15d2cc904674a7092a8a569118f67e2011b
Tiago Papile <tiagopapile@gmail.com>
2021-05-12 11:10:03 -0300

    Verifica acf

--- Blocks/Plugins/RemoteData/Assets/remote-fields.js
+++ Blocks/Plugins/RemoteData/Assets/remote-fields.js
@@ -1,4 +1,7 @@
 (function($) {
+	if(typeof acf == 'undefined')
+		return;
+	
     acf.add_filter('select2_args', function(args, $select, settings, field, instance) {
         // do something to args
         if(field.data('name') == 'fields')
