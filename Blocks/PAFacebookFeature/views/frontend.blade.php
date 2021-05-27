f3af7f1fc03d9a1ab27690b5d17f28e968fe9038
Lorde Aleister <lordealeister@gmail.com>
2021-05-27 13:03:45 -0300

    Corrige largura de blocos

--- Blocks/PAFacebookFeature/views/frontend.blade.php
+++ Blocks/PAFacebookFeature/views/frontend.blade.php
@@ -2,7 +2,7 @@
 	<img class="img-preview" src="{{ get_template_directory_uri() }}/Blocks/PAFacebookFeature/preview.png" />
 @else
 	<script async defer crossorigin="anonymous" src="https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v10.0" nonce="ytfDnIE0"></script>
-	<div class="pa-widget pa-w-carousel-feature col col-md-4 mb-5">
+	<div class="pa-widget pa-w-carousel-feature mb-5">
 		<h2>{{ $title ?? 'Widget - Facebook - Feature' }}</h2>
 		<div class="fb-page" data-href={{ $url }} data-tabs="timeline" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>
 	</div>
