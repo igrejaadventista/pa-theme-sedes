46fd7a527b8c7943b47f12ff8aa8329521cf196f
Arthur Aires <aaires@studiovisual.com.br>
2021-06-01 14:46:43 -0300

    Criado componente de categorias

--- Blocks/PACategoriaFeature/PACategoriaFeature.php
+++ Blocks/PACategoriaFeature/PACategoriaFeature.php
@@ -3,15 +3,9 @@
 namespace Blocks\PACategoriaFeature;
 
 use Blocks\Block;
-use Blocks\Fields\Source;
-
-use WordPlate\Acf\ConditionalLogic;
-use WordPlate\Acf\Fields\Accordion;
-use WordPlate\Acf\Fields\Image;
-use WordPlate\Acf\Fields\Link;
 use WordPlate\Acf\Fields\Relationship;
-use WordPlate\Acf\Fields\Repeater;
 use WordPlate\Acf\Fields\Select;
+use WordPlate\Acf\Fields\Taxonomy;
 use WordPlate\Acf\Fields\Text;
 use WordPlate\Acf\Fields\Textarea;
 
@@ -20,46 +14,58 @@ use WordPlate\Acf\Fields\Textarea;
  */
 class PACategoriaFeature extends Block {
 
-    public function __construct() {
+	public function __construct() {
 		// Set block settings
-        parent::__construct([
-            'title' 	  => 'Categorias - Feature',
-            'description' => 'Categorias',
-            'category' 	  => 'widgets',
-            'post_types'  => ['post', 'page'],
-			'keywords' 	  => ['category', 'select'],
-			'icon' 		  => '<svg id="Icons" style="enable-background:new 0 0 32 32;" version="1.1" viewBox="0 0 32 32" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><style type="text/css">
+		parent::__construct( [
+			'title'       => 'Categorias - Feature',
+			'description' => 'Categorias',
+			'category'    => 'widgets',
+			'post_types'  => [ 'post', 'page' ],
+			'keywords'    => [ 'category', 'select' ],
+			'icon'        => '<svg id="Icons" style="enable-background:new 0 0 32 32;" version="1.1" viewBox="0 0 32 32" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><style type="text/css">
 								.st0{fill:none;stroke:#000000;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;}
 								</style><polyline class="st0" points="25,11 27,13 25,15 "/><polyline class="st0" points="7,11 5,13 7,15 "/><path class="st0" d="M29,23H3c-1.1,0-2-0.9-2-2V5c0-1.1,0.9-2,2-2h26c1.1,0,2,0.9,2,2v16C31,22.1,30.1,23,29,23z"/><circle class="st0" cx="16" cy="28" r="1"/><circle class="st0" cx="10" cy="28" r="1"/><circle class="st0" cx="22" cy="28" r="1"/></svg>',
-            // Other valid acf_register_block() settings
-        ]);
-    }
-	
+			// Other valid acf_register_block() settings
+		] );
+	}
+
 	/**
 	 * setFields Register ACF fields with WordPlate/Acf lib
 	 *
 	 * @return array Fields array
 	 */
 	protected function setFields(): array {
-		return 
+		$categories    = \get_categories( [
+			'orderby' => 'name',
+			'order'   => 'ASC'
+		] );
+		$selectChoices = [];
+
+		if ( ! empty( $categories ) ):
+			foreach ( $categories as $category ):
+				$selectChoices[ $category->id ] = $category->name;
+			endforeach;
+		endif;
+
+		return
 			[
-				Text::make('Título', 'title'),
-				Select::make('', 'categories'),
+				Text::make( 'Título', 'title' ),
+				Select::make( 'Selecionar categorias', 'categories' )
+				      ->choices( $selectChoices )
+				      ->returnFormat( 'array' )
+				      ->allowMultiple(),
 			];
 	}
-	    
-    /**
-     * with Inject fields values into template
-     *
-     * @return array
-     */
-    public function with(): array {		
-        return [
-            'title'  => field('title'),
-			'categories' => \get_categories([
-				'orderby' => 'name',
-				'order' => 'ASC'
-			]),
-        ];
-    }
+
+	/**
+	 * with Inject fields values into template
+	 *
+	 * @return array
+	 */
+	public function with(): array {
+		return [
+			'title'      => field( 'title' ),
+			'categories' => option( 'categories' ),
+		];
+	}
 }
--- Blocks/PACategoriaFeature/views/frontend.blade.php
+++ Blocks/PACategoriaFeature/views/frontend.blade.php
@@ -1,5 +1,18 @@
-<div class="pa-widget pa-w-carousel-feature col col-md-4 mb-5">
+<div class="pa-widget pa-w-categories-feature col col-md-4 mb-5">
 	<h2>{{ $title ?? 'Widget - Categoria - Feature' }}</h2>
+	@notempty($categories)
 	<div class="mt-4">
+		@foreach($categories as $category)
+			<h3>{{ $category['name'] }} </h3>
+			{{--				@notempty($category['icon'])--}}
+			{{--				<img class="rounded img-fluid" src="{{ $slide['thumbnail']['url'] }}"--}}
+			{{--					 alt="{{ $slide['thumbnail']['alt'] ?: 'Imagem do slide ' . $loop->iteration }}"/>--}}
+			{{--				@endnotempty--}}
+
+			{{--				@notempty($category['title'])--}}
+			{{--				<h3 class="card-title font-weight-bold h5 mt-4 pa-truncate-1">{!! $slide['title'] !!}</h3>--}}
+			{{--				@endnotempty--}}
+		@endforeach
 	</div>
+	@endnotempty
 </div>
