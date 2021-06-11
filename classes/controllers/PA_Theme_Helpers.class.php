d6fd09f9c5e148d8b291e3435b147ad2d442874a
Eli Mendonca <contato@elimendonca.com>
2021-06-11 15:31:17 -0300

    Atualiza bootstrap p/ a versão 5.0.1

--- classes/controllers/PA_Theme_Helpers.class.php
+++ classes/controllers/PA_Theme_Helpers.class.php
@@ -64,7 +64,7 @@ class PaThemeHelpers {
 	}
 
 	function registerAssets() {
-		wp_enqueue_style( 'bootstrap-style', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css', null, null);
+		wp_enqueue_style( 'bootstrap-style', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css', null, null);
 		wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Noto+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap', null, null);
 		wp_enqueue_style( 'pa-theme-sedes-style', get_template_directory_uri(). '/style.css', null, null);
 		wp_enqueue_style( 'pa-theme-sedes-print', get_template_directory_uri() . '/print.css', null, null);
