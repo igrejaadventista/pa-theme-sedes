ed2f43b8c19de732828a14e6622141af997b8d92
tiagopapile <tiagopapile@gmail.com>
2021-08-27 10:42:04 -0300

    Atualiza composer

--- functions.php
+++ functions.php
@@ -5,6 +5,11 @@ use Blocks\Blocks;
 if(file_exists($composer = __DIR__. '/vendor/autoload.php'))
 	require_once $composer;
 
+// Customize the url setting to fix incorrect asset URLs.
+add_filter('acf/settings/url', function() {
+    return get_template_directory_uri() . '/vendor/wp-plugins/advanced-custom-fields-pro/';
+});
+
 new Blocks;
 
 require_once (dirname(__FILE__) . '/classes/controllers/PA_Theme_Helpers.class.php');
