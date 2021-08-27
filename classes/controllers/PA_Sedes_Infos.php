9cc5626162a305f7b3cafa74f05a3b70933af92c
tiagopapile <tiagopapile@gmail.com>
2021-08-27 14:50:36 -0300

    Altera funções para obter dados das opções de multisite ou do tema

--- classes/controllers/PA_Sedes_Infos.php
+++ classes/controllers/PA_Sedes_Infos.php
@@ -1,8 +1,8 @@
 <?php
 
-function get_info_sedes(){
-
-	$sedes_sede = get_field('ct_headquarter', 'option');
+function get_info_sedes() {
+	$network = empty(get_field('overwrite_global_settings', 'option')) ? '_network' : '';
+	$sedes_sede = get_field("ct_headquarter{$network}", 'option');
 
 	// if(!$sedes_sede){
 	// 	$blog_id = get_main_site_id();
@@ -15,86 +15,86 @@ function get_info_sedes(){
 }
 
 function get_info_adress(){
+	$network = empty(get_field('overwrite_global_settings', 'option')) ? '_network' : '';
+	$sedes_adress = get_field("ct_adress{$network}", 'option');
 
-	$sedes_adress = get_field('ct_adress', 'option');
-
-	if(!$sedes_adress){
-		$blog_id = get_main_site_id();
-		switch_to_blog($blog_id);
-			$sedes_adress = get_field('ct_adress', 'option');
-		restore_current_blog();
-	}
+	// if(!$sedes_adress){
+	// 	$blog_id = get_main_site_id();
+	// 	switch_to_blog($blog_id);
+	// 		$sedes_adress = get_field('ct_adress', 'option');
+	// 	restore_current_blog();
+	// }
 
 	return $sedes_adress;
 }
 
 function get_info_telephone(){
+	$network = empty(get_field('overwrite_global_settings', 'option')) ? '_network' : '';
+	$sedes_telephone = get_field("ct_telephone{$network}", 'option');
 
-	$sedes_telephone = get_field('ct_telephone', 'option');
-
-	if(!$sedes_telephone){
-		$blog_id = get_main_site_id();
-		switch_to_blog($blog_id);
-			$sedes_telephone = get_field('ct_telephone', 'option');
-		restore_current_blog();
-	}
+	// if(!$sedes_telephone){
+	// 	$blog_id = get_main_site_id();
+	// 	switch_to_blog($blog_id);
+	// 		$sedes_telephone = get_field('ct_telephone', 'option');
+	// 	restore_current_blog();
+	// }
 
 	return $sedes_telephone;
 }
 
 
 function get_info_facebook(){
+	$network = empty(get_field('overwrite_global_settings', 'option')) ? '_network' : '';
+	$sedes_facebook = get_field("sn_facebook{$network}", 'option');
 
-	$sedes_facebook = get_field('sn_facebook', 'option');
-
-	if(!$sedes_facebook){
-		$blog_id = get_main_site_id();
-		switch_to_blog($blog_id);
-			$sedes_facebook = get_field('sn_facebook', 'option');
-		restore_current_blog();
-	}
+	// if(!$sedes_facebook){
+	// 	$blog_id = get_main_site_id();
+	// 	switch_to_blog($blog_id);
+	// 		$sedes_facebook = get_field('sn_facebook', 'option');
+	// 	restore_current_blog();
+	// }
 
 	return $sedes_facebook;
 }
 
 function get_info_twitter(){
+	$network = empty(get_field('overwrite_global_settings', 'option')) ? '_network' : '';
+	$sedes_twitter = get_field("sn_twitter{$network}", 'option');
 
-	$sedes_twitter = get_field('sn_twitter', 'option');
-
-	if(!$sedes_twitter){
-		$blog_id = get_main_site_id();
-		switch_to_blog($blog_id);
-			$sedes_twitter = get_field('sn_twitter', 'option');
-		restore_current_blog();
-	}
+	// if(!$sedes_twitter){
+	// 	$blog_id = get_main_site_id();
+	// 	switch_to_blog($blog_id);
+	// 		$sedes_twitter = get_field('sn_twitter', 'option');
+	// 	restore_current_blog();
+	// }
 
 	return $sedes_twitter;
 }
 
 function get_info_youtube(){
+	$network = empty(get_field('overwrite_global_settings', 'option')) ? '_network' : '';
+	$sedes_youtube = get_field("sn_youtube{$network}", 'option');
 
-	$sedes_youtube = get_field('sn_youtube', 'option');
-
-	if(!$sedes_youtube){
-		$blog_id = get_main_site_id();
-		switch_to_blog($blog_id);
-			$sedes_youtube = get_field('sn_youtube', 'option');
-		restore_current_blog();
-	}
+	// if(!$sedes_youtube){
+	// 	$blog_id = get_main_site_id();
+	// 	switch_to_blog($blog_id);
+	// 		$sedes_youtube = get_field('sn_youtube', 'option');
+	// 	restore_current_blog();
+	// }
 
 	return $sedes_youtube;
 }
 
 function get_info_instagram(){
+	$network = empty(get_field('overwrite_global_settings', 'option')) ? '_network' : '';
+	$sedes_instagram = get_field("sn_instagram{$network}", 'option');
 
-	$sedes_instagram = get_field('sn_instagram', 'option');
-
-	if(!$sedes_instagram){
-		$blog_id = get_main_site_id();
-		switch_to_blog($blog_id);
-			$sedes_instagram = get_field('sn_instagram', 'option');
-		restore_current_blog();
-	}
+	// if(!$sedes_instagram){
+	// 	$blog_id = get_main_site_id();
+	// 	switch_to_blog($blog_id);
+	// 		$sedes_instagram = get_field('sn_instagram', 'option');
+	// 	restore_current_blog();
+	// }
 
 	return $sedes_instagram;
 }
