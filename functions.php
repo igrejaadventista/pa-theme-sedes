4bc5f3a6a315a08c28e6e7151531d9e5afc10d22
tiagopapile <tiagopapile@gmail.com>
2021-08-31 16:47:26 -0300

    Reaorganiza autoload

--- functions.php
+++ functions.php
@@ -3,9 +3,11 @@
 if(file_exists($composer = __DIR__. '/vendor/autoload.php'))
 	require_once $composer;
 
-require_once (dirname(__FILE__) . '/vendor/wp-plugins/advanced-custom-fields-pro/acf.php');
-require_once (dirname(__FILE__) . '/vendor/studiovisual/acf-gutenblocks/acf-gutenblocks.php');
-require_once (dirname(__FILE__) . '/vendor/lordealeister/acf-multisite-options/acf-multisite-options.php');
+// Customize the url setting to fix incorrect asset URLs.
+add_filter('acf/settings/url', function() {
+    return get_template_directory_uri() . '/vendor/wp-plugins/advanced-custom-fields-pro/';
+});
+
 require_once (dirname(__FILE__) . '/classes/controllers/PA_Theme_Helpers.class.php');
 require_once (dirname(__FILE__) . '/classes/controllers/PA_ACF_Helpers.class.php');
 require_once (dirname(__FILE__) . '/classes/controllers/PA_ACF_Site-settings.class.php');
@@ -19,19 +21,31 @@ require_once (dirname(__FILE__) . '/classes/controllers/PA_Register_Sidebars.cla
 require_once (dirname(__FILE__) . '/classes/controllers/PA_Header_Title.class.php');
 require_once (dirname(__FILE__) . '/classes/controllers/PA_Sedes_Infos.php');
 require_once (dirname(__FILE__) . '/classes/widgets/PA_Widget_Apps.class.php');
-require_once (dirname(__FILE__) . '/core/PA_Theme_Sedes_Install.php'); // CORE INSTALL
+// CORE INSTALL
+require_once (dirname(__FILE__) . '/core/PA_Theme_Sedes_Install.php');
 
 new Blocks\Blocks;
-
-// Customize the url setting to fix incorrect asset URLs.
-add_filter('acf/settings/url', function() {
-    return get_template_directory_uri() . '/vendor/wp-plugins/advanced-custom-fields-pro/';
-});
 
 function pa_wp_custom_menus() {
 	register_nav_menu('pa-menu-default', __( 'PA - Menu - Default' ));
 }
 add_action( 'init', 'pa_wp_custom_menus' );
+
+
+
+//Função auxiliar para imprimir no console o print_r.
+// function pconsole($var) {
+
+// 	$s = json_encode($var);
+// 	echo "<script>console.log(". $s . ");</script>";
+// 	return;
+// }
+// Função auxiliar para imprimir no console o echo.
+// function cconsole($var) {
+
+// 	echo "<script>console.log('" . $var . "');</script>";
+// 	return;
+// }
 
 if(file_exists(get_stylesheet_directory() . '/classes/PA_Directives.php'))
     require_once(get_stylesheet_directory() . '/classes/PA_Directives.php');
