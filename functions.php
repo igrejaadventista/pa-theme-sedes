315978d07d58816ffa792642ee37f7901fadd2ca
tiagopapile <tiagopapile@gmail.com>
2021-08-31 14:18:30 -0300

    Reorganiza load

--- functions.php
+++ functions.php
@@ -1,17 +1,11 @@
 <?php 
 
-use Blocks\Blocks;
-
 if(file_exists($composer = __DIR__. '/vendor/autoload.php'))
 	require_once $composer;
 
-// Customize the url setting to fix incorrect asset URLs.
-add_filter('acf/settings/url', function() {
-    return get_template_directory_uri() . '/vendor/wp-plugins/advanced-custom-fields-pro/';
-});
-
-new Blocks;
-
+require_once (dirname(__FILE__) . '/vendor/wp-plugins/advanced-custom-fields-pro/acf.php');
+require_once (dirname(__FILE__) . '/vendor/studiovisual/acf-gutenblocks/acf-gutenblocks.php');
+require_once (dirname(__FILE__) . '/vendor/lordealeister/acf-multisite-options/acf-multisite-options.php');
 require_once (dirname(__FILE__) . '/classes/controllers/PA_Theme_Helpers.class.php');
 require_once (dirname(__FILE__) . '/classes/controllers/PA_ACF_Helpers.class.php');
 require_once (dirname(__FILE__) . '/classes/controllers/PA_ACF_Site-settings.class.php');
@@ -24,32 +18,20 @@ require_once (dirname(__FILE__) . '/classes/controllers/PA_Page_Numbers.class.ph
 require_once (dirname(__FILE__) . '/classes/controllers/PA_Register_Sidebars.class.php');
 require_once (dirname(__FILE__) . '/classes/controllers/PA_Header_Title.class.php');
 require_once (dirname(__FILE__) . '/classes/controllers/PA_Sedes_Infos.php');
-
 require_once (dirname(__FILE__) . '/classes/widgets/PA_Widget_Apps.class.php');
+require_once (dirname(__FILE__) . '/core/PA_Theme_Sedes_Install.php'); // CORE INSTALL
 
-// CORE INSTALL
-require_once (dirname(__FILE__) . '/core/PA_Theme_Sedes_Install.php');
+new Blocks\Blocks;
+
+// Customize the url setting to fix incorrect asset URLs.
+add_filter('acf/settings/url', function() {
+    return get_template_directory_uri() . '/vendor/wp-plugins/advanced-custom-fields-pro/';
+});
 
 function pa_wp_custom_menus() {
 	register_nav_menu('pa-menu-default', __( 'PA - Menu - Default' ));
 }
 add_action( 'init', 'pa_wp_custom_menus' );
-
-
-
-//Função auxiliar para imprimir no console o print_r.
-// function pconsole($var) {
-
-// 	$s = json_encode($var);
-// 	echo "<script>console.log(". $s . ");</script>";
-// 	return;
-// }
-// Função auxiliar para imprimir no console o echo.
-// function cconsole($var) {
-
-// 	echo "<script>console.log('" . $var . "');</script>";
-// 	return;
-// }
 
 if(file_exists(get_stylesheet_directory() . '/classes/PA_Directives.php'))
     require_once(get_stylesheet_directory() . '/classes/PA_Directives.php');
