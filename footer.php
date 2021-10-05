b5ca2c15ea4dc896612c49911ff579edb93523a6
Eli Mendonça <internetdsa@gmail.com>
2021-10-05 20:48:11 -0300

    Atualiza bootstrap p/ a versão 5.1.2

--- footer.php
+++ footer.php
@@ -8,8 +8,7 @@
 	</div>
 
 <!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<?php wp_footer(); ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script><?php wp_footer(); ?>
 
 </body>
 
