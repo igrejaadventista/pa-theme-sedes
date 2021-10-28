241f9a5bfdd2cd35499de04e2b4444128d08807f
webdsa <internetdsa@gmail.com>
2021-10-28 14:17:07 -0300

    -- Ajustando rolagem do slider do video

--- assets/scripts/_pa-slider-videos.js
+++ assets/scripts/_pa-slider-videos.js
@@ -1,38 +1,36 @@
 /* Scripsts Slider Videos */
-import Glide from '@glidejs/glide';
+import Glide from "@glidejs/glide";
 
 export function pa_slider_videos() {
-	var nodes = document.querySelectorAll('.pa-glide-videos');
+  var nodes = document.querySelectorAll(".pa-glide-videos");
 
-	if(!nodes.length)
-		return;
+  if (!nodes.length) return;
 
-	nodes.forEach(function(node) {
-		var glide = new Glide(node, {
-			type: "carousel",
-			perView: 5,
-			startAt: 0,
-			gap: 24,
-			hoverpause: true,
-			//autoplay: 2500,
-			breakpoints: {
-				1024: {
-					perView: 3,
-				},
-				800: {
-					perView: 2,
-				},
-				480: {
-					perView: 1,
-					gap: 8,
-					peek: {
-						before: 0,
-						after: 129,
-					},
-				},
-			},
-		});
-	
-		glide.mount();
-	});
+  nodes.forEach(function(node) {
+    var glide = new Glide(node, {
+      type: "carousel",
+      touchAngle: 120,
+      perView: 5,
+      startAt: 0,
+      gap: 24,
+      breakpoints: {
+        1024: {
+          perView: 3
+        },
+        800: {
+          perView: 2
+        },
+        480: {
+          perView: 1,
+          gap: 8,
+          peek: {
+            before: 0,
+            after: 129
+          }
+        }
+      }
+    });
+
+    glide.mount();
+  });
 }
