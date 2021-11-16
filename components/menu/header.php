579992cd0040ce49e12ec54cc9d91a0c1bd95f70
webdsa <71404981+webdsa@users.noreply.github.com>
2021-11-16 11:37:40 -0300

    Corrige ponto que estava sobrando

--- components/menu/header.php
+++ components/menu/header.php
@@ -3,7 +3,7 @@ $campo = get_info_sedes();
 $menu_global = PaThemeHelpers::getGlobalMenu('global-header');
 $relative_site = network_site_url("", "relative");
 $relative_sites = [
-  '/'.   => 'PT',
+  '/'    => 'PT',
   '/pt/' => 'PT', 
   '/es/' => 'ES'
 ];
