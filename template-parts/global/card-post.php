236138deeedcfac1b3a9bdd78b4b11a43bdfa95b
webdsa <internetdsa@gmail.com>
2021-11-15 22:37:29 -0300

    -- Ajustando proporção das imagens e abertura do php

--- template-parts/global/card-post.php
+++ template-parts/global/card-post.php
@@ -1,17 +1,17 @@
-<?php if(isset($args['post']) && !empty($args['post'])): ?>
+<?php if (isset($args['post']) && !empty($args['post'])) : ?>
   <div class="pa-blog-item mb-4 mb-md-4 border-0">
     <a href="<?= get_the_permalink($args['post']->ID) ?>" title="<?= get_the_title($args['post']->ID) ?>">
       <div class="row">
-        <?php if(has_post_thumbnail($args['post'])): ?>
+        <?php if (has_post_thumbnail($args['post'])) : ?>
           <div class="col-5 col-md-4">
             <div class="ratio ratio-16x9">
               <figure class="figure m-xl-0">
-                <img src="<?= check_immg($args['post']->ID, 'medium') ?>" class="figure-img img-fluid rounded m-0 h-100 w-100" alt="<?= get_the_title($args['post']->ID) ?>">
+                <img src="<?= check_immg($args['post']->ID, 'medium') ?>" class="figure-img img-fluid rounded m-0 h-100 w-100 object-cover" alt="<?= get_the_title($args['post']->ID) ?>">
 
-                <?php if(isset($args['category']) && !empty($args['category'])): ?>
+                <?php if (isset($args['category']) && !empty($args['category'])) : ?>
                   <figcaption class="pa-img-tag figure-caption text-uppercase rounded-right d-none d-md-table-cell"><?= $args['category'] ?></figcaption>
                 <?php endif; ?>
-              </figure>	
+              </figure>
             </div>
           </div>
 
@@ -19,7 +19,7 @@
 
         <div class="col">
           <div class="card-body <?= has_post_thumbnail($args['post']) ? 'p-0' : 'ps-4 pe-0 py-4 border-start border-5 pa-border' ?>">
-            <?php if(isset($$args['format']) && !empty($$args['format'])): ?>
+            <?php if (isset($$args['format']) && !empty($$args['format'])) : ?>
               <span class="pa-tag text-uppercase d-none d-xl-table-cell rounded"><?= $$args['format'] ?></span>
             <?php endif; ?>
 
--- template-parts/global/feature.php
+++ template-parts/global/feature.php
@@ -1,4 +1,4 @@
-<?php if(isset($args['post']) && !empty($args['post'])): ?>
+<?php if (isset($args['post']) && !empty($args['post'])) : ?>
   <div class="pa-blog-itens mb-5">
     <h2 class="mb-4"><?= _e('Featured', 'iasd') ?></h2>
 
@@ -6,13 +6,13 @@
       <a href="<?= get_the_permalink($args['post']->ID) ?>" title="<?php get_the_title($args['post']->ID) ?>">
         <div class="ratio ratio-16x9">
           <figure class="figure m-xl-0 w-100">
-            <img src="<?= check_immg($args['post']->ID, 'full') ?>" class="figure-img img-fluid m-0 rounded w-100 h-100" alt="<?= get_the_title($args['post']->ID) ?>">
+            <img src="<?= check_immg($args['post']->ID, 'full') ?>" class="figure-img img-fluid m-0 rounded w-100 h-100 object-cover" alt="<?= get_the_title($args['post']->ID) ?>">
 
             <figcaption class="figure-caption position-absolute w-100 p-3 rounded-bottom">
-              <?php if(isset($args['tag']) && !empty($args['tag'])): ?>
+              <?php if (isset($args['tag']) && !empty($args['tag'])) : ?>
                 <span class="pa-tag rounded-1 text-uppercase mb-2 d-none d-md-table-cell px-2"><?= $args['tag']; ?></span>
               <? endif; ?>
-            
+
               <h3 class="h5 pt-2 pa-truncate-2"><?= get_the_title($args['post']->ID) ?></h3>
             </figcaption>
           </figure>
@@ -20,4 +20,4 @@
       </a>
     </div>
   </div>
-<? endif; ?>
+<?php endif; ?>
