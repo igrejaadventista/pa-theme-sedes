fd335f8e00fc608d3e1adb0b7b01a06e3d473331
Meirinaldo Júnior <meirinaldo.junior0@gmail.com>
2021-12-01 20:53:03 -0300

    fix: get network options

--- classes/controllers/PA_Sedes_Infos.php
+++ classes/controllers/PA_Sedes_Infos.php
@@ -1,8 +1,8 @@
 <?php
 
 function get_info_sedes() {
-	$network = empty(get_field('overwrite_global_settings', 'option')) ? '_network' : '';
-	return get_field("ct_headquarter{$network}", 'option');
+	$network = empty(Plugin::getInstance()->get_options_page_by_post_id('pa_network_options')) ? '_network' : '';
+	return get_field("ct_headquarter{$network}", 'pa_network_options');
 }
 
 function get_info_adress(){
