74411a0f0841c6faeaf8948f0bea26571a63995d
mizael isaltino <internetdsa@gmail.com>
2022-06-28 14:55:02 -0300

    Colocando condição que desabilita script cado a teg não seja encontrada.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## [1.5.14] - 2022-06-28
+
+### Fix
+
+- Colocando condição que desabilita script cado a teg não seja encontrada.
+
 ## [1.5.13] - 2022-04-28
 
 ### Fix
--- classes/controllers/PA_Strong_Passwords.php
+++ classes/controllers/PA_Strong_Passwords.php
@@ -124,5 +124,5 @@ add_action('admin_head', 'no_weak_password_header');
 function no_weak_password_header()
 {
   echo "<style>.pw-weak{display:none!important}</style>";
-  echo '<script>document.getElementById("pw-checkbox").disabled = true;</script>';
+  echo '<script>const pwCheckbox = document.getElementById("pw-checkbox"); if(pwCheckbox){ pwCheckbox.disabled = true;}</script>';
 }
