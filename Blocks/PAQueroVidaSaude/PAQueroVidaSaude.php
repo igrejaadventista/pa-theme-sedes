d74ec64bfbc2efa4ba534cfb2e0575ec6004fd20
Tiago Papile <tpapile@studiovisual.com.br>
2022-07-21 10:40:04 -0300

    advc-17 Ajusta api

--- Blocks/PAQueroVidaSaude/PAQueroVidaSaude.php
+++ Blocks/PAQueroVidaSaude/PAQueroVidaSaude.php
@@ -31,7 +31,7 @@ class PAQueroVidaSaude extends Block {
 	 */
 	protected function setFields(): array {
 
-		$api = "https://". API_PA ."/qvs/". LANG ."/pa-blocks";
+		$api = "https://". API_PA ."/qvs/". LANG ."/posts";
 
 		return array_merge(
 			[
