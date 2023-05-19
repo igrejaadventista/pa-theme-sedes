b697850d84e0cadb5de0720003429356ffe8e6aa
Leandro OC <oliva1337@gmail.com>
2023-05-19 08:31:57 -0300

    Revert "refatoração métodos wordplateACF para suporte php 8.2"
    This reverts commit 00efac7d046d2f7641cbc0b1cc70487cdb8c8146.

--- Extended/LocalData.php
+++ Extended/LocalData.php
@@ -1,128 +1,129 @@
 <?php
 
-namespace ExtendedLocal;
+namespace Extended;
 
-use Extended\ACF\Fields\Field;
-use Extended\ACF\Fields\Settings\ConditionalLogic;
-use Extended\ACF\Fields\Settings\Wrapper;
-use Extended\ACF\Fields\Settings\Instructions;
+use WordPlate\Acf\Fields\Field;
+use WordPlate\Acf\Fields\Attributes\ConditionalLogic;
+use WordPlate\Acf\Fields\Attributes\Instructions;
+use WordPlate\Acf\Fields\Attributes\Wrapper;
 
 /**
  * Register new local data field
  */
 class LocalData extends Field {
 
-  use Wrapper;
-  use Instructions;
-  use ConditionalLogic;
+    use ConditionalLogic;
+    use Instructions;
+    use Wrapper;
 
-  protected null|string $type = 'localposts_data';
+    protected $type = 'localposts_data';
+
+	/**
+	 * initialLimit Initial number of items to be returned
+	 *
+	 * @param  int $value Items limit
+	 * @return self
+	 */
+	public function initialLimit(int $value): self {
+        $this->config->set('limit', $value);
+
+        return $this;
+    }
+
+	/**
+	 * postTypes Post types to query
+	 *
+	 * @param  array $values Post types array
+	 * @return self
+	 */
+	public function postTypes(array $values): self {
+        $this->config->set('post_type', $values);
+
+        return $this;
+    }
+
+	/**
+	 * manualFields Additional manual fields
+	 *
+	 * @param  array $values Fields array
+	 * @return self
+	 */
+	public function manualFields(array $values): self {
+        $this->config->set('sub_fields', $values);
+
+        return $this;
+    }
+
+	/**
+	 * hideFields Hide fixed fields, except title and link
+	 *
+	 * @param  array $values Fields array
+	 * @return self
+	 */
+	public function hideFields(array $values): self {
+        $this->config->set('hide_fields', $values);
+
+        return $this;
+    }
+
+	/**
+	 * ManualItems Enable/disable manual items
+	 *
+	 * @param  bool $value Status
+	 * @return self
+	 */
+	public function manualItems(bool $value): self {
+        $this->config->set('manual_items', intval($value));
+
+        return $this;
+    }
+
+	/**
+	 * searchFilter Enable/disable search
+	 *
+	 * @param  bool $value Status
+	 * @return self
+	 */
+	public function searchFilter(bool $value): self {
+        $this->config->set('search_filter', intval($value));
+
+        return $this;
+    }
+
+	/**
+	 * limitFilter Enable/disable limit
+	 *
+	 * @param  bool $value Status
+	 * @return self
+	 */
+	public function limitFilter(bool $value): self {
+        $this->config->set('limit_filter', intval($value));
+
+        return $this;
+    }
+
+	/**
+	 * canSticky Enable/disable sticky button
+	 *
+	 * @param  bool $value Status
+	 * @return self
+	 */
+	public function canSticky(bool $value): self {
+        $this->config->set('can_sticky', intval($value));
+
+        return $this;
+    }
 
   /**
-   * Set the initial limit of items to be returned.
-   *
-   * @param int $value Items limit
-   * @return self
-   */
-  public function initialLimit(int $value): self {
-    $this->withSettings(['limit' => $value]);
+	 * filterTaxonomies Set which taxonomies will be available in filters
+	 *
+	 * @param  array $values Taxonomies slugs
+	 * @return self
+	 */
+	public function filterTaxonomies(array $values): self {
+    $this->config->set('taxonomies', $values);
 
     return $this;
   }
 
-  /**
-   * Set the post types to query.
-   *
-   * @param array $values Post types array
-   * @return self
-   */
-  public function postTypes(array $values): self {
-    $this->withSettings(['post_type' => $values]);
-
-    return $this;
-  }
-
-  /**
-   * Set additional manual fields.
-   *
-   * @param array $values Fields array
-   * @return self
-   */
-  public function manualFields(array $values): self {
-    $this->withSettings(['sub_fields' => $values]);
-
-    return $this;
-  }
-
-  /**
-   * Hide fixed fields, except title and link.
-   *
-   * @param array $values Fields array
-   * @return self
-   */
-  public function hideFields(array $values): self {
-    $this->withSettings(['hide_fields' => $values]);
-
-    return $this;
-  }
-
-  /**
-   * Enable/disable manual items.
-   *
-   * @param bool $value Status
-   * @return self
-   */
-  public function manualItems(bool $value): self {
-    $this->withSettings(['manual_items' => intval($value)]);
-
-    return $this;
-  }
-
-  /**
-   * Enable/disable search filter.
-   *
-   * @param bool $value Status
-   * @return self
-   */
-  public function searchFilter(bool $value): self {
-    $this->withSettings(['search_filter' => intval($value)]);
-
-    return $this;
-  }
-
-  /**
-   * Enable/disable limit filter.
-   *
-   * @param bool $value Status
-   * @return self
-   */
-  public function limitFilter(bool $value): self {
-    $this->withSettings(['limit_filter' => intval($value)]);
-
-    return $this;
-  }
-
-  /**
-   * Enable/disable sticky button.
-   *
-   * @param bool $value Status
-   * @return self
-   */
-  public function canSticky(bool $value): self {
-    $this->withSettings(['can_sticky' => intval($value)]);
-
-    return $this;
-  }
-
-  /**
-   * Set which taxonomies will be available in filters.
-   *
-   * @param array $values Taxonomies slugs
-   * @return self
-   */
-  public function filterTaxonomies(array $values): self {
-    $this->withSettings(['taxonomies' => $values]);
-
-    return $this;
-  }
 }
