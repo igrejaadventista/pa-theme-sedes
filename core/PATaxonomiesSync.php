6d72e80101d85bc2f685520db99e584bfa010e0a
Mizael Isaltino <mizael.isaltino@gmail.com>
2023-05-24 22:46:24 -0300

    Ajustando a frequência de acionamento do cron: blocos (30 minutos), menus banner global e taxonomias (Uma vez por dia).

--- core/PATaxonomiesSync.php
+++ core/PATaxonomiesSync.php
@@ -27,7 +27,7 @@ class PATaxonomiesSync {
 
   public function __construct() {
     if(!wp_next_scheduled('PA-Service_Taxonomy_Schedule'))
-      wp_schedule_event(time(), '20min', 'PA-Service_Taxonomy_Schedule'); // Register cron schedule event
+      wp_schedule_event(time(), 'daily', 'PA-Service_Taxonomy_Schedule'); // Register cron schedule event
 
     add_action('PA-Service_Taxonomy_Schedule', array($this, 'sync')); // Register cron
     add_action('wp_ajax_sync/taxonomies', array($this, 'sync')); // Register ajax handler
