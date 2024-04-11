3a7850c67a82edd4382a831fd912472930fa631f
Samir Cafruni <samircafruni@gmail.com>
2024-04-11 10:26:53 -0300

    Define qual logotipo será usado pelo tema.

--- components/menu/header-logo.php
+++ components/menu/header-logo.php
@@ -1,7 +1,12 @@
 <?php $sede = getSiteInfo(); ?>
+<?php $custom_logo = wp_get_attachment_image_src( get_theme_mod( 'custom_logo' ) , 'full' ); ?>
+<?php $default_logo = get_template_directory_uri() . "/assets/sedes/" . LANG . "/" . $sede['ct_headquarter']->slug . ".svg" ?>
 
 <div class="col-auto d-flex align-items-center pa-header-logo">
   <a href="<?= get_home_url(); ?>" class="w-auto h-100">
-    <img src="<?= get_template_directory_uri() . "/assets/sedes/" . LANG . "/" . $sede['ct_headquarter']->slug . ".svg" ?>" alt="<?= !empty($sede['ct_headquarter']->name) ? $sede['ct_headquarter']->name : '' ?>" title="<?= !empty($sede['ct_headquarter']->name) ? $sede['ct_headquarter']->name : '' ?>" class="h-100 w-auto pa-logo-iasd">
+    <img src="<?= $custom_logo ? $custom_logo[0] : $default_logo ?>" 
+         alt="<?= !empty($sede['ct_headquarter']->name) ? $sede['ct_headquarter']->name : '' ?>" 
+         title="<?= !empty($sede['ct_headquarter']->name) ? $sede['ct_headquarter']->name : '' ?>" 
+         class="h-100 w-auto pa-logo-iasd">
   </a>
 </div>
