eb7a47937b64699d5b5306364a74ab439a08d24d
Victor Guimarães <vguimaraes@studiovisual.com.br>
2024-10-31 15:43:25 -0300

    Ajusta exibição dos conteúdos no bloco 7Class

--- Blocks/PA7Class/PA7Class.php
+++ Blocks/PA7Class/PA7Class.php
@@ -3,10 +3,9 @@
 namespace Blocks\PA7Class;
 
 use Blocks\Block;
-// use ExtendedLocal\RemoteData;
+use ExtendedLocal\RemoteData;
 use Fields\MoreContent;
 use Extended\ACF\Fields\Text;
-use Extended\ACF\Fields\Select;
 
 /**
  * Class PA7Class
@@ -31,37 +30,20 @@ class PA7Class extends Block {
 	 * @return array Fields array
 	 */
 	protected function setFields(): array {
-
-		$response = wp_remote_get('https://api.adventistas.dev/7class/pt');
-
-		if (is_array($response) && !is_wp_error($response)) {
-			$json_text = html_entity_decode($response['body']);
-			$classes = json_decode($json_text, true);
-			$classes = $classes[0]['acf']['classes'];
-			// echo '<pre>';
-			// var_dump($classes);
-			// echo '</pre>';
-		}
-
-		$choices = [];
-		foreach ($classes as $key => $class) {
-			$choices[sanitize_title($class['curso']) . '_' . $key] = $class['curso'];
-		}
-
-		// var_dump(['forest_green' => 'Forest Green', 'sky_blue' => 'Sky Blue']);
-		// var_dump($choices);
+		$api = "https://" . API_PA . "/7class/" . LANG;
 
 		return array_merge(
 			[
 				Text::make(__('Title', 'iasd'), 'title')
-					->defaultValue(__('IASD - 7 Class', 'iasd')),
-				Select::make('Cursos')
-					->choices($choices)
-					// ->format('value')
-					// ->default('forest_green')
-					// ->multiple()
-					// ->stylized()
-					// ->lazyLoad(),
+					->defaultValue(__('IASD - 7Class', 'iasd')),
+
+				RemoteData::make(__('Itens', 'iasd'), 'items')
+					->endpoints([
+						$api .' > Posts',
+					])
+					->initialLimit(5)
+					->getFields(['featured_media_url', 'terms'])
+					->hideFields(['excerpt'])
 			],
 			MoreContent::make()
 		);
